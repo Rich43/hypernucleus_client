4867430cbc32b062208e76fc0ad807610b54c411
Richie Ward <RichieS@GMail.com>
2011-07-23 19:19:21 +0100

    Added obligatory setup.py script. Did a chmod +x on startup script.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from distutils.core import setup
+
+setup(name='hypernucleus',
+      version='0.1',
+      description='Python Games Database',
+      author='Richie Ward',
+      author_email='RichieS@GMail.com',
+      url='http://hypernucleus.pynguins.com',
+      packages=['hypernucleus'],
+     )
