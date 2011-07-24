982135419ce81a517f0567652cf1eedce89544fe
Richie Ward <RichieS@GMail.com>
2011-07-24 01:31:33 +0100

    Fix resources

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	pyrcc4 -py3 -o hypernucleus/view/icons.py hypernucleus/view/icons.qrc
--- hypernucleus/__init__.py
+++ hypernucleus/__init__.py
@@ -1,9 +1,10 @@
 import sys
 from PyQt4 import Qt
 from hypernucleus.controller.main import MainWindow
-
+from hypernucleus.view.icons import qInitResources
 def main():
     app = Qt.QApplication(sys.argv)
+    qInitResources()
     main_window = MainWindow()
     main_window.show()
     sys.exit(app.exec_())
