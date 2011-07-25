56d65e7367090658acf332018d2d165cdd0a0915
Richie Ward <RichieS@GMail.com>
2011-07-25 19:32:00 +0100

    Add some primitive connectors.

--- hypernucleus/__init__.py
+++ hypernucleus/__init__.py
@@ -2,6 +2,7 @@ import sys
 from PyQt4 import Qt
 from hypernucleus.controller.main import MainWindow
 from hypernucleus.view.icons import qInitResources
+
 def main():
     app = Qt.QApplication(sys.argv)
     qInitResources()
--- hypernucleus/controller/main.py
+++ hypernucleus/controller/main.py
@@ -6,7 +6,8 @@ Created on 23 Jul 2011
 
 from hypernucleus.view import main_path
 from PyQt4.QtGui import QMainWindow
-from PyQt4 import uic
+from PyQt4 import uic, QtCore
+import sys
 
 class MainWindow(QMainWindow):
     """
@@ -15,3 +16,37 @@ class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         self.ui = uic.loadUi(main_path, self)
+        self.q_connect("actionExit", "exit")
+        self.q_connect("actionRun", "run")
+        self.q_connect("actionStop", "stop")
+        self.q_connect("actionUninstall", "uninstall")
+        self.q_connect("actionSettings", "settings")
+        
+    def q_connect(self, action_name, method_name):
+        """
+        'Quick Connect'
+        Use connect with less code :)
+        """
+        self.connect(getattr(self.ui, action_name), 
+                     QtCore.SIGNAL('triggered()'), 
+                     self, QtCore.SLOT(method_name + '()'))
+        
+    @QtCore.pyqtSlot()
+    def exit(self):
+        sys.exit()
+
+    @QtCore.pyqtSlot()
+    def run(self):
+        print("Method 'run' executed.")
+    
+    @QtCore.pyqtSlot()
+    def stop(self):
+        print("Method 'stop' executed.")
+        
+    @QtCore.pyqtSlot()
+    def uninstall(self):
+        print("Method 'uninstall' executed.")
+        
+    @QtCore.pyqtSlot()
+    def settings(self):
+        print("Method 'settings' executed.")
