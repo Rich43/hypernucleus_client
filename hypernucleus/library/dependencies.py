d4bda73028096406930abe943a516f4e7a768d9b
Richie Ward <RichieS@GMail.com>
2011-07-26 02:02:07 +0100

    Module Spelling mistake

--- hypernucleus/library/dependencies.py
+++ hypernucleus/library/dependencies.py
@@ -1,4 +1,4 @@
-#PyGameDB, A database of games made in python with automatic installation.
+#hypernucleus, A database of games made in python with automatic installation.
 #Copyright (C) 2008  Richie Ward + Contributors.
 
 #This program is free software; you can redistribute it and/or
