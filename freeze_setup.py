8c60d903c5e319a6d6156c13a7206906585584c2
Richie Ward <RichieS@GMail.com>
2011-08-18 01:08:36 +0100

    Switch to include files

--- freeze_setup.py
+++ freeze_setup.py
@@ -1,13 +1,14 @@
 import sys
 import glob
 from cx_Freeze import setup, Executable
+from os import sep
 from os.path import join
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-zip_includes = glob.glob(join("hypernucleus", "view", "*.ui"))
-includes = []
+include_files = glob.glob(join("hypernucleus", "view", "*.ui"))
+include_files = [(p, sep.join(p.split(sep)[1:])) for p in include_files]
 excludes = []
 packages = []
 
@@ -18,4 +19,4 @@ setup(name = "Hypernucleus",
       url='http://hypernucleus.pynguins.com',
       executables = [Executable("run_hypernucleus.py", base = base)],
       options = {'build_exe': {'excludes': excludes, 'packages': packages, 
-                               'zip_includes': zip_includes}})
+                               'include_files': include_files}})
