1b462643a42fdb06e6957fff4f150ff7e52c1b97
Richie Ward <RichieS@GMail.com>
2016-11-14 08:07:19 +0000

    Improve binary build script Make sure pycurl is imported during build

--- build_linux.sh
+++ build_linux.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-FILENAME=hypernucleus_10_32.tar.bz2
+FILENAME=hypernucleus_10_64.tar.bz2
+rm $FILENAME
+rm -rf build
 make
 python3 freeze_setup.py build
 cd build
-mv * hypernucleus
-tar cjvf $FILENAME hypernucleus
+mv * hn
+tar cjvf $FILENAME hn
 cp $FILENAME ../
 cd ..
 rm -rf build
