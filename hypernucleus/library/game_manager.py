d159e15536c1172dc2df87d54b88430c4485e0cb
Richie Ward <RichieS@GMail.com>
2016-11-20 10:02:07 +0000

    Reworked game manager to use absoloute paths. Remove unneeded import from run_hypernucleus.py

--- hypernucleus/library/game_manager.py
+++ hypernucleus/library/game_manager.py
@@ -11,35 +11,51 @@ class GameManager:
     """
     Manages Game Processes
     """
+	
     gamelist = {}
     
-    def __init__(self):
-        self.apppath = sys.argv[0]
-    
+    def get_apppath(self):
+        """
+        Get path to current application, useful for Popen.
+        """
+        def remove_dup(seq):
+            seen = set()
+            seen_add = seen.add
+            return [x for x in seq if not (x in seen or seen_add(x))]
+        if sys.argv[0].startswith("./"):
+            no_dot_slash = sys.argv[0].replace("./", "")
+            apppath.append(join(os.getcwd(), no_dot_slash))
+        else:
+            apppath = sys.argv
+        if sys.executable:
+            apppath = [sys.executable] + apppath
+        apppath = remove_dup(apppath)
+        return apppath
+        
     def execute_game(self, game_name):
         """
         Run the game itself.
         """
         p = Paths()
-        
+
         # Load XML File
         ini_data = INIManager()
         xml_data = JsonModel(ini_data.get_xml_url())
-         
+
         # Change directory to the game's folder
         os.chdir(join(p.games, game_name))
-        
+
         # Get various needed data
         game_ins_ver = ini_data.get_installed_version(game_name)
-        game_module_type = xml_data.get_revision_module_type(game_name, 
+        game_module_type = xml_data.get_revision_module_type(game_name,
                                                              GAME, game_ins_ver)
-        
+
         # Figure out if its a file or folder module
         if game_module_type == "file":
             sys.path.append(join(p.games, game_name))
         else:
             sys.path.append(p.games)
-        
+
         # Add dependencies to path
         for dep_name, dep_ver in \
                     xml_data.list_dependencies_recursive(game_name, GAME):
@@ -49,13 +65,13 @@ class GameManager:
                 sys.path.append(join(p.dependencies, dep_name))
             else:
                 sys.path.append(p.dependencies)
-            
+
         # Run it
         game = __import__(game_name)
-        
+
         if hasattr(game, "main"):
             game.main()
-    
+
     def run_game(self, game_name):
         """
         Run the game optional argument.
@@ -63,7 +79,9 @@ class GameManager:
         # Remove stopped games.
         self.cleanup()
         # Start game.
-        process = Popen([self.apppath, "-r", game_name])
+        path = self.get_apppath()
+        path.extend(["-r", game_name])
+        process = Popen(path)
         # Add process to dictionary.
         self.gamelist[game_name] = process
 
--- run_hypernucleus.py
+++ run_hypernucleus.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 import hypernucleus
-import pycurl
 hypernucleus.main()
